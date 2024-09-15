8c1d8cf6a125d603ad56131aa3a9b3fb1ae85cbe
Creewie <tabero2006@gmail.com>
2024-09-15 12:48:23 +0200

    Zmiana true na tak oraz false na nie

--- modules/task.js
+++ modules/task.js
@@ -5,7 +5,7 @@ function createTask(desc){
     var newTask = {
         id: id,
         desc: desc,
-        done: false
+        done: 'nie'
     }
     tasks.push(newTask)
     id++
@@ -14,7 +14,7 @@ function createTask(desc){
 function finishTask(id){
     var task = tasks.find(e => e.id == id)
     if(task){
-        task.done = true
+        task.done = 'tak'
     }
     else{
         console.log(`Nastąpił bład przy wczytaniu zadanie o id ${id}`)
